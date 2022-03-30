8f640d68fdfc58454e40afc3a65c79a7ee14e6b3
unknown <aisa.smajic27@gmail.com>
2022-03-30 20:21:29 +0200

    variable

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+       var x;
+       x = 8;
+
+    // Only change above this line
+    return x;
+
+}
+console.log(main());
+module.exports = main;
