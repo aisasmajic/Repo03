c76a9a7522077bc711013973c6f9656dacf676b3
unknown <aisa.smajic27@gmail.com>
2022-03-30 18:09:32 +0200

    Adding comment

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code below this line
+    // This is a single line JavaScript comment
+
+    /* document.write("This is a multi-line comment!");
+     document.write("This is a multi-line comment!");
+      document.write("This is a multi-line comment!");
+       document.write("This is a multi-line comment!");
+       */
+
+    // Only changes code abowe this line  
+    console.log(main()) ;
+}
+
+
+
+module.exports = main;
